ae1c636ab7cfacf6c27084f03f034c05996f755c
CrypticDylo <103567897+CrypticDylo@users.noreply.github.com>
2022-11-02 11:27:59 +0200

    Added Report Animal Screen and color value for button

---
+++ app/src/main/java/com/example/guidinglight_opsc_part_2_group_5/ReportAnimal.java
@@ -0,0 +1,14 @@
+package com.example.guidinglight_opsc_part_2_group_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportAnimal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_animal);
+    }
+}
